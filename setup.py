2c5d8d881af8eecbc22c6fe2356f50763f4abd51
Aditya Prajapati <adityaprajapati.9358.no@gmail.com>
2024-05-23 16:34:59 +0530

    First set up commit

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages,setup
+from typing import List
+
+
+
+def get_requirements(file_path:str)->List[str]:
+    """this function will return the list of requirements """
+
+    requirements=[]
+
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace('\n',"") for req in requirements]
+
+
+    return requirements
+
+
+
+
+
+
+
+
+setup(
+    name='mlprojects',
+    version='0.0.1',
+    author='Aditya',
+    author_email='adityaprajapati.9358.no@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
